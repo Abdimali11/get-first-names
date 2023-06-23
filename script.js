e7d6afdb2055c526c9b37704bc8a6ed943fc17ad
Abdimalik11 <ilkacaze5522@gmail.com>
2023-06-23 10:43:22 -0700

    this is the answer of get-first-names

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 // Good Luck 💪🏾
+
+
+function getFirstNames(names){
+    return names.map(names =>names[0])
+}
+let names = [["Ahmed","Aisha","Amina"],["Jaamac","Khadijah","Layla"],["Mohamed","Ali"]]
+let firstnames=getFirstNames(names);
+console.log(firstnames);
+
